a78a6d2602c7af6a7433868fa3c685452c30b475
rmasnun <rmasnun1@student.gsu.edu>
2024-04-29 14:27:12 -0400

    First Commit

---
+++ Database/create_database.php
@@ -0,0 +1,378 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "amohan2";
+$password = "amohan2";
+$dbname = "amohan2";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create structure and insert data
+$sql = "-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2020 at 11:34 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = \"NO_AUTO_VALUE_ON_ZERO\";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = \"+00:00\";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `developers`
+--
+CREATE DATABASE IF NOT EXISTS `developers` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `developers`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `about`
+--
+
+DROP TABLE IF EXISTS `about`;
+CREATE TABLE `about` (
+  `id` int(10) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` longtext NOT NULL,
+  `image` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `about`
+--
+
+INSERT INTO `about` (`id`, `title`, `content`, `image`) VALUES
+(10, 'About Our Company', '<div id=\"pgc-w5d0dcc3394ac1-0-0\" class=\"panel-grid-cell\">\r\n<div id=\"panel-w5d0dcc3394ac1-0-0-0\" class=\"so-panel widget widget_sow-editor panel-first-child panel-last-child\" data-index=\"0\">\r\n<div class=\"so-widget-sow-editor so-widget-sow-editor-base\">\r\n<div class=\"siteorigin-widget-tinymce textwidget\">\r\n<p class=\"text_all_p_tag_css\">Pramukh Web Solution is a website design and Web development Company dedicated to provide web based solutions to all type of businesses. Located in Ahmedabad, Gujarat (India).</p>\r\n<p class=\"text_all_p_tag_css\">Pramukh Web Solution is a one stop solution for all your IT needs. It Provides full featured innovative and high quality cost effective IT web designing solutions ranging from customized web development, PHP web development, ASP.NET and all kind of programming to complete web solutions including web design, Web Development in PHP, eCommerce Solutions, Multimedia, and Print publication solutions, CSS/XHTML Web Design, Content Management, SEO (Web Promotion), Domain Registration, Web Hosting to businesses throughout the India.</p>\r\n<div id=\"pgc-w5d0dcc3394ac1-0-0\" class=\"panel-grid-cell\">\r\n<div id=\"panel-w5d0dcc3394ac1-0-0-0\" class=\"so-panel widget widget_sow-editor panel-first-child panel-last-child\" data-index=\"0\">\r\n<div class=\"so-widget-sow-editor so-widget-sow-editor-base\">\r\n<div class=\"siteorigin-widget-tinymce textwidget\">\r\n<p class=\"text_all_p_tag_css\">Pramukh Web Solution is a website design and Web development Company dedicated to provide web based solutions to all type of businesses. Located in Ahmedabad, Gujarat (India).</p>\r\n<p class=\"text_all_p_tag_css\">Pramukh Web Solution is a one stop solution for all your IT needs. It Provides full featured innovative and high quality cost effective IT web designing solutions ranging from customized web development, PHP web development, ASP.NET and all kind of programming to complete web solutions including web design, Web Development in PHP, eCommerce Solutions, Multimedia, and Print publication solutions, CSS/XHTML Web Design, Content Management, SEO (Web Promotion), Domain Registration, Web Hosting to businesses throughout the India.</p>\r\n</div>\r\n</div>\r\n</div>\r\n</div>\r\n</div>\r\n</div>\r\n</div>\r\n</div>', 'condos-pool.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `aid` int(10) NOT NULL,
+  `auser` varchar(50) NOT NULL,
+  `aemail` varchar(50) NOT NULL,
+  `apass` varchar(50) NOT NULL,
+  `adob` date NOT NULL,
+  `aphone` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`aid`, `auser`, `aemail`, `apass`, `adob`, `aphone`) VALUES
+(2, 'disha', 'disha@gmail.com', 'disha', '1999-02-02', '9689689698'),
+(6, 'final', 'final@gmail.com', 'final', '2020-04-29', '7979656578'),
+(7, 'test', 'test@gmail.com', 'test', '2020-04-29', '8997979765'),
+(8, 'check', 'check@gmail.com', 'check', '2020-04-29', '8979785688'),
+(9, 'admin', 'admin@gmail.com', 'admin', '1999-12-06', '9878786545');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+DROP TABLE IF EXISTS `city`;
+CREATE TABLE `city` (
+  `cid` int(50) NOT NULL,
+  `cname` varchar(100) NOT NULL,
+  `sid` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `city`
+--
+
+INSERT INTO `city` (`cid`, `cname`, `sid`) VALUES
+(9, 'navi mumbai', 3),
+(10, 'vapi', 2),
+(11, 'valsad', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE `contact` (
+  `cid` int(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `message` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`cid`, `name`, `email`, `phone`, `subject`, `message`) VALUES
+(2, 'demo', 'demo@gmail.com', '9765989689', 'demo', 'demo'),
+(4, 'test', 'test@gmail.com', '7976976979', 'test', 'test'),
+(5, 'final', 'final@gmail.com', '7697967967', 'final', 'final'),
+(6, 'disha', 'disha@gmail.com', '7898797696', 'demo', 'demo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+DROP TABLE IF EXISTS `feedback`;
+CREATE TABLE `feedback` (
+  `fid` int(50) NOT NULL,
+  `uid` int(50) NOT NULL,
+  `fdescription` varchar(300) NOT NULL,
+  `status` int(1) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`fid`, `uid`, `fdescription`, `status`, `date`) VALUES
+(2, 15, 'Magicbricks made my life easy. It helped me with the search for my first ever investment i.e. 1BHK apartment in Mira Road. Thanks to the team for providing relevant tools like EMI calculator and smart search.\r\n', 1, '2020-04-03'),
+(3, 18, 'I am young professional, Magicbricks search helped me in shortlisting property in Navi Mumbai. I learned what kind of property will cost me how much and what are the types of amenities I will be getting?', 1, '2020-04-03'),
+(4, 21, 'I was looking for a flat in Andheri and Magicbricks website helped me get one smoothly. I could choose not just the property but also check what others had to say about the area. The site is simple and user friendly.\r\n', 1, '2020-04-03'),
+(5, 23, 'The constant touch through other true calls really surprised me.They sent their officer to get the photographs of my shop & promptly posted all the pics which helped me in getting the tenant fast. Hats off to Magicbricks.\r\n', 1, '2020-04-03'),
+(6, 24, 'I moved to Mumbai from Delhi early this year and I looked online for a suitable apartment for rent in areas near my workplace in Andheri. Thanks Magicbricks for giving me so many options with Travel Time search.\r\n', 0, '2020-04-03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `property`
+--
+
+DROP TABLE IF EXISTS `property`;
+CREATE TABLE `property` (
+  `pid` int(50) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `pcontent` longtext NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `bhk` varchar(50) NOT NULL,
+  `stype` varchar(100) NOT NULL,
+  `bedroom` int(50) NOT NULL,
+  `bathroom` int(50) NOT NULL,
+  `balcony` int(50) NOT NULL,
+  `kitchen` int(50) NOT NULL,
+  `hall` int(50) NOT NULL,
+  `floor` varchar(50) NOT NULL,
+  `size` int(50) NOT NULL,
+  `price` int(50) NOT NULL,
+  `location` varchar(200) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `feature` longtext NOT NULL,
+  `pimage` varchar(300) NOT NULL,
+  `pimage1` varchar(300) NOT NULL,
+  `pimage2` varchar(300) NOT NULL,
+  `pimage3` varchar(300) NOT NULL,
+  `pimage4` varchar(300) NOT NULL,
+  `uid` int(50) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `mapimage` varchar(300) NOT NULL,
+  `topmapimage` varchar(300) NOT NULL,
+  `groundmapimage` varchar(300) NOT NULL,
+  `totalfloor` varchar(50) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `state`
+--
+
+DROP TABLE IF EXISTS `state`;
+CREATE TABLE `state` (
+  `sid` int(50) NOT NULL,
+  `sname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `state`
+--
+
+INSERT INTO `state` (`sid`, `sname`) VALUES
+(2, 'gujarat'),
+(3, 'goa'),
+(4, 'maharashtra'),
+(7, 'bihar'),
+(9, 'chhattisgarh'),
+(10, 'uttar pardesh'),
+(15, 'rajasthan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `uid` int(50) NOT NULL,
+  `uname` varchar(100) NOT NULL,
+  `uemail` varchar(100) NOT NULL,
+  `uphone` varchar(20) NOT NULL,
+  `upass` varchar(50) NOT NULL,
+  `utype` varchar(50) NOT NULL,
+  `uimage` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`uid`, `uname`, `uemail`, `uphone`, `upass`, `utype`, `uimage`) VALUES
+(14, 'admin', 'admin@gmail.com', '9876543210', 'admin', 'user', '3.jpg'),
+(15, 'aryan', 'aryan@gmail.com', '9878678678', 'aryan', 'agent', '2.jpg'),
+(16, 'demo', 'demo@gmail.com', '7976976979', 'demo', 'user', '1.jpg'),
+(21, 'disha', 'disha@gmail.com', '7976956956', 'disha', 'agent', '2.jpg'),
+(22, 'disha', 'disha1@gmail.com', '8997897869', 'disha', 'agent', '1.jpg'),
+(23, 'testing', 'testing@gmail.com', '9869596597', 'testing', 'builder', '1.jpg'),
+(24, 'some', 'some@gmail.com', '9689698697', 'some', 'builder', '3.jpg'),
+(25, 'test', 'test12@gmail.com', '9798678969', 'test', 'builder', 'avatar-3.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `about`
+--
+ALTER TABLE `about`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`aid`);
+
+--
+-- Indexes for table `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`fid`);
+
+--
+-- Indexes for table `property`
+--
+ALTER TABLE `property`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `state`
+--
+ALTER TABLE `state`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `about`
+--
+ALTER TABLE `about`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `city`
+--
+ALTER TABLE `city`
+  MODIFY `cid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `cid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `fid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `property`
+--
+ALTER TABLE `property`
+  MODIFY `pid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `state`
+--
+ALTER TABLE `state`
+  MODIFY `sid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `uid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+";
+
+//Checking whether it worked or not
+if ($conn->query($sql)===true){
+    echo "database populated successfully";
+} else {
+    echo "Error creating table " . $conn->error;
+}
+
+// Close connection
+$conn->close();
+?>
---
+++ creditcard.php
@@ -0,0 +1,198 @@
+<?php
+ini_set('session.cache_limiter','public');
+session_cache_limiter(false);
+session_start();
+include("config.php");
+$error="";
+$msg="";
+if(isset($_REQUEST['add']))
+{
+	$cardNumber=$_REQUEST['cardNumber'];
+	$expiryDate=$_REQUEST['expiryDate'];
+	$cvv=$_REQUEST['cvv'];
+	$billingAddress=$_REQUEST['billingAddress'];
+	$phoneNumber=$_REQUEST['phoneNumber'];
+  $userid = $_SESSION['uid'];
+
+	$query = "SELECT * FROM cards where cardNumber='$cardNumber'";
+	$res=mysqli_query($con, $query);
+	$num=mysqli_num_rows($res);
+
+	if($num == 1)
+	{
+		$error = "<p class='alert alert-warning'>This card is already used</p> ";
+	}
+	else
+	{
+
+		if(!empty($cardNumber) && !empty($expiryDate) && !empty($cvv) && !empty($billingAddress) && !empty($phoneNumber))
+		{
+
+			$sql="INSERT INTO cards (ucardNumber, userid, uexpiryDate,ucvv,ubillingAddress,utype,uphoneNumber) VALUES ('$cardNumber', '$userid', '$expiryDate','$cvv','$billingAddress','$utype','$phoneNumber')";
+			$result=mysqli_query($con, $sql);
+			   if($result){
+				   $msg = "<p class='alert alert-success'>Card Added Successfully</p> ";
+           header("Location: profile.php");
+			   }
+			   else{
+				   $error = "<p class='alert alert-warning'>Card additon unsuccessful. Please try again later</p> ";
+			   }
+		}else{
+			$error = "<p class='alert alert-warning'>Please Fill all the fields</p>";
+		}
+	}
+
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Meta Tags -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="shortcut icon" href="images/favicon.ico">
+
+<!--	Fonts
+	========================================================-->
+<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+
+<!--	Css Link
+	========================================================-->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
+<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="css/layerslider.css">
+<link rel="stylesheet" type="text/css" href="css/color.css">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/login.css">
+
+<!--	Title
+	=========================================================-->
+<title>Homex - Real Estate Template</title>
+</head>
+<body>
+
+<!--	Page Loader
+=============================================================
+<div class="page-loader position-fixed z-index-9999 w-100 bg-white vh-100">
+	<div class="d-flex justify-content-center y-middle position-relative">
+	  <div class="spinner-border" role="status">
+		<span class="sr-only">Loading...</span>
+	  </div>
+	</div>
+</div>
+-->
+
+
+<div id="page-wrapper">
+    <div class="row">
+        <!--	Header start  -->
+		<?php include("include/header.php");?>
+        <!--	Header end  -->
+
+        <!--	Banner   --->
+        <div class="banner-full-row page-banner" style="background-image:url('images/breadcromb.jpg');">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="page-name float-left text-white text-uppercase mt-1 mb-0"><b>ADD Card info</b></h2>
+                    </div>
+                    <div class="col-md-6">
+                        <nav aria-label="breadcrumb" class="float-left float-md-right">
+                            <ol class="breadcrumb bg-transparent m-0 p-0">
+                                <li class="breadcrumb-item text-white"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">Register</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+         <!--	Banner   --->
+
+
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">Credit Card Information</h4>
+            <form method="post" enctype="multipart/form-data">
+              <div class="form-group">
+                <label for="cardNumber">Card Number</label>
+                <input name="cardNumber" type="text" class="form-control" id="cardNumber" placeholder="Enter card number" autocomplete="off">
+                <small id="cardType" class="form-text text-muted"></small>
+              </div>
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="expiryDate">Expiration Date</label>
+                  <input name="expiryDate" type="text" class="form-control" id="expiryDate" placeholder="MM/YY">
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="cvv">CVV</label>
+                  <input name="cvv" type="text" class="form-control" id="cvv" placeholder="CVV">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="billingAddress">Billing Address</label>
+                <input name="billingAddress" type="text" class="form-control" id="billingAddress" placeholder="Enter billing address">
+              </div>
+              <div class="form-group">
+                <label for="phoneNumber">Phone Number</label>
+                <input name="phoneNumber" type="text" class="form-control" id="phoneNumber" placeholder="Enter phone number">
+              </div>
+              <button name="add" value="add" type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script>
+    // Function to detect credit card type based on card number
+    function detectCardType(cardNumber) {
+      var cardType = '';
+      // Array of card types and their corresponding patterns
+      var cardPatterns = [
+        { type: 'Visa', pattern: /^4/ },
+        { type: 'MasterCard', pattern: /^5[1-5]/ },
+        { type: 'American Express', pattern: /^3[47]/ },
+        { type: 'Discover', pattern: /^6(?:011|5[0-9]{2})/ },
+        { type: 'Diners Club', pattern: /^3(?:0[0-5]|[68][0-9])/ },
+        { type: 'JCB', pattern: /^(?:2131|1800|35\d{3})/ }
+      ];
+      // Loop through card patterns to find a match
+      cardPatterns.forEach(function (card) {
+        if (card.pattern.test(cardNumber)) {
+          cardType = card.type;
+        }
+      });
+      return cardType;
+    }
+
+    // Function to update card type display
+    function updateCardTypeDisplay() {
+      var cardNumberInput = document.getElementById('cardNumber');
+      var cardTypeDisplay = document.getElementById('cardType');
+      var cardNumber = cardNumberInput.value.replace(/\s/g, ''); // Remove spaces
+      var cardType = detectCardType(cardNumber);
+      cardTypeDisplay.textContent = cardType !== '' ? 'Card Type: ' + cardType : '';
+    }
+
+    // Event listener for card number input
+    document.getElementById('cardNumber').addEventListener('input', updateCardTypeDisplay);
+  </script>
+</body>
+</html>
+
+
